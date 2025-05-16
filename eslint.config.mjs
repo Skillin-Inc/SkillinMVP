2cb47f4469418ba8362e0e09199a95cbe49f4deb
Henry Pham <hqpham98@gmail.com>
2025-05-15 17:24:35 -0700

    ESLint Update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,20 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pluginPromise from "eslint-plugin-promise";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
+    plugins: {
+      promise: pluginPromise,
+    },
+  },
+  {
     rules: {
-      "func-style": ["error", "declaration"],
+      "func-style": ["error", "declaration", { allowArrowFunctions: true }],
+      "promise/prefer-await-to-then": ["error"],
     },
   }
 );
