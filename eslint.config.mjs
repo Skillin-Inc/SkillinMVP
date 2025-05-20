e712f017b9b93c698999753e7158830b17e306e7
Henry Pham <hqpham98@gmail.com>
2025-05-19 19:38:40 -0700

    Added eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,13 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 import pluginPromise from "eslint-plugin-promise";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
     plugins: {
       promise: pluginPromise,
