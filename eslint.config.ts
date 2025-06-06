c344376051db2458392aee2e97b0360e0df41d01
Arnav Patel <patelarnavm+github@gmail.com>
2025-06-05 19:21:32 -0700

    added configs to main

---
+++ eslint.config.ts
@@ -0,0 +1,21 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginPromise from "eslint-plugin-promise";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      promise: pluginPromise as any,
+    },
+  },
+  {
+    rules: {
+      "func-style": ["error", "declaration", { allowArrowFunctions: true }],
+      "promise/prefer-await-to-then": "error",
+    },
+  }
+);
