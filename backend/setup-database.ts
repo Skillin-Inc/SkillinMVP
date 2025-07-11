34039c300f0dcdf5a3af4d0878929b8deaed9912
Arnav Patel <patelarnavm+github@gmail.com>
2025-07-10 22:45:00 -0700

    fix(db): add default categories back, update setupdb to use url, update backend packages

--- backend/setup-database.ts
+++ backend/setup-database.ts
@@ -5,11 +5,7 @@ import path from "path";
 import "dotenv/config";
 async function setupDatabase() {
   const pool = new Pool({
-    host: process.env.PG_HOST || "localhost",
-    port: Number(process.env.PG_PORT) || 5432,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+    connectionString: process.env.DATABASE_URL,
   });
 
   try {
