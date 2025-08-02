9c7bc84d62436892b1dcae56d35fa1e600e18606
Henry Pham <hqpham98@gmail.com>
2025-08-02 02:55:06 -0700

    Update videoUpload.ts

--- backend/src/routes/videoUpload.ts
+++ backend/src/routes/videoUpload.ts
@@ -4,22 +4,25 @@ const router = Router();
 const CLOUDFLARE_API_TOKEN = process.env.CLOUDFLARE_API_TOKEN;
 const CLOUDFLARE_ACCOUNT_ID = process.env.CLOUDFLARE_ACCOUNT_ID;
 
+type ClientUploadRequest = {
+  userID: string;
+  fileSize: string;
+  fileName: string;
+};
+
 //Resumable Upload Endpoint
 router.post("/", async (req: Request, res: Response) => {
   try {
+    const { userID, fileSize, fileName }: ClientUploadRequest = req.body;
     const endpoint = `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/stream?direct_user=true`;
-
-    // Testing Purposes
-    const metadata = `name ${Buffer.from("video.mp4").toString("base64")}`;
+    const metadata = `name ${Buffer.from(fileName).toString("base64")}`;
 
     // Type-safe Header Construction
     const headers = new Headers();
     headers.append("Authorization", `Bearer ${CLOUDFLARE_API_TOKEN}`);
     headers.append("Tus-Resumable", "1.0.0");
-
-    //Testing purposes
-    headers.append("Upload-Length");
-
+    headers.append("Upload-Creator", userID);
+    headers.append("Upload-Length", fileSize);
     headers.append("Upload-Metadata", metadata);
 
     const response = await fetch(endpoint, {
